cf3e6ab439676f7963d9796f73f19a8af64b8f40
Phillip Stevens <phillip.stevens@gmail.com>
2018-02-22 22:27:57 +1100

    check existence of CP/M drive before selecting - 2

--- cpm22/yabios_def.asm
+++ cpm22/yabios_def.asm
@@ -9,79 +9,79 @@ PUBLIC _bank_cpm_bdos_addr
 DEFC _bank_cpm_bdos_addr             = $0006
 
 PUBLIC _f_mount
-DEFC _f_mount                        = $5C07
+DEFC _f_mount                        = $5BFC
 
 PUBLIC _f_open
-DEFC _f_open                         = $5C74
+DEFC _f_open                         = $5C69
 
 PUBLIC _f_read
-DEFC _f_read                         = $6192
+DEFC _f_read                         = $6187
 
 PUBLIC _f_write
-DEFC _f_write                        = $66CF
+DEFC _f_write                        = $66C4
 
 PUBLIC _f_sync
-DEFC _f_sync                         = $6CBA
+DEFC _f_sync                         = $6CAF
 
 PUBLIC _f_close
-DEFC _f_close                        = $6E1D
+DEFC _f_close                        = $6E12
 
 PUBLIC _f_chdir
-DEFC _f_chdir                        = $6E53
+DEFC _f_chdir                        = $6E48
 
 PUBLIC _f_getcwd
-DEFC _f_getcwd                       = $6F0A
+DEFC _f_getcwd                       = $6EFF
 
 PUBLIC _f_lseek
-DEFC _f_lseek                        = $7182
+DEFC _f_lseek                        = $7177
 
 PUBLIC _f_opendir
-DEFC _f_opendir                      = $7726
+DEFC _f_opendir                      = $771B
 
 PUBLIC _f_closedir
-DEFC _f_closedir                     = $781D
+DEFC _f_closedir                     = $7812
 
 PUBLIC _f_readdir
-DEFC _f_readdir                      = $7843
+DEFC _f_readdir                      = $7838
 
 PUBLIC _f_stat
-DEFC _f_stat                         = $78B4
+DEFC _f_stat                         = $78A9
 
 PUBLIC _f_getfree
-DEFC _f_getfree                      = $7921
+DEFC _f_getfree                      = $7916
 
 PUBLIC _f_truncate
-DEFC _f_truncate                     = $7BE4
+DEFC _f_truncate                     = $7BD9
 
 PUBLIC _f_unlink
-DEFC _f_unlink                       = $7E0A
+DEFC _f_unlink                       = $7DFF
 
 PUBLIC _f_mkdir
-DEFC _f_mkdir                        = $7FAE
+DEFC _f_mkdir                        = $7FA3
 
 PUBLIC _f_rename
-DEFC _f_rename                       = $8212
+DEFC _f_rename                       = $8207
 
 PUBLIC _f_chmod
-DEFC _f_chmod                        = $852E
+DEFC _f_chmod                        = $8523
 
 PUBLIC _f_utime
-DEFC _f_utime                        = $85D9
+DEFC _f_utime                        = $85CE
 
 PUBLIC _f_expand
-DEFC _f_expand                       = $86A0
+DEFC _f_expand                       = $8695
 
 PUBLIC _f_gets
-DEFC _f_gets                         = $8AFD
+DEFC _f_gets                         = $8AF2
 
 PUBLIC _f_putc
-DEFC _f_putc                         = $8D33
+DEFC _f_putc                         = $8D28
 
 PUBLIC _f_puts
-DEFC _f_puts                         = $8D66
+DEFC _f_puts                         = $8D5B
 
 PUBLIC _f_printf
-DEFC _f_printf                       = $8DBE
+DEFC _f_printf                       = $8DB3
 
 PUBLIC _free_fastcall
 DEFC _free_fastcall                  = $03D8
