f107ee6b9480fa042e88cca4ec2fc57f82231197
Phillip Stevens <phillip.stevens@gmail.com>
2018-09-30 12:03:29 +1000

    MP/M - mpmldr reversed records, not bytes - working

--- mpm21/mpmldr/mpmldr.c
+++ mpm21/mpmldr/mpmldr.c
@@ -8,6 +8,7 @@
 
 #define PAGE0_SIZE 0x100        /* size of a Page0 copy buffer (on heap) */
 #define SYSDAT_SIZE 0x100
+#define RECORD_SIZE 0x080
 
 extern void *memcpy_far( void *str_dest, char bank_dest, void *str_src, char bank_src, size_t n);
 extern void jp_far( void *str, int8_t bank);
@@ -23,16 +24,18 @@ int argc;
 char ** argv;
 {
     FILE *fptr;
-    char c;
 
     uint8_t sysdat_page;
     uint8_t init_page;
+
+    uint16_t mpm_records;
     
     uint8_t * data_addr;
 
-    uint8_t * load_data;        /* point to data */
     uint8_t * page0_template;   /* pointer to Page 0 template */
 
+    uint8_t * load_data;        /* point to data */
+    uint16_t * load_data_uint;  /* pointer to word data */    
 
     if( argc == 1 )             /* no arguments on command line */
     {
@@ -63,38 +66,43 @@ char ** argv;
 
         sysdat_page = load_data[0];
         init_page = load_data[11];
-        printf("SYSDAT %u INIT %u\n", sysdat_page, init_page);
+        
+        load_data_uint = (uint16_t *)load_data;
+        mpm_records = load_data_uint[60]; /* records in bytes 120-121 */
+
+        printf("SYSDAT %x INIT %x RECORDS %d\n", sysdat_page, init_page, mpm_records );
         
         data_addr = (uint8_t *)(load_data[0]*0x100);
         
         /* copy from current Bank into Bank 8, from address in file */
-        memcpy_far(data_addr, bank_get_rel(8), load_data, 0, (sizeof(uint8_t)*SYSDAT_SIZE));
+        memcpy_far(data_addr, -(bank_get_rel(8)), load_data, 0, (sizeof(uint8_t)*SYSDAT_SIZE));
 
         free(load_data);
     }
 
-    fseek(fptr, 0, 0);      /* Rewind file for testing */
-
-    /* Read contents from file */
+    /* Read rest of MPM.SYS contents from file */
     
-    while( (c = fgetc(fptr)) != EOF )
-    { 
-        --data_addr;
-        load_data[((uint16_t)data_addr)%SYSDAT_SIZE] = c; /* store the bytes in reverse order */
-        
-        if( ((uint16_t)data_addr)%SYSDAT_SIZE == 0 )
+    do {
+    
+        fread(load_data, (sizeof(uint8_t)), RECORD_SIZE, fptr);
+
+        if( ferror(fptr) != 0 )
         {
-            /* copy from current Bank into Bank 8, from address in file */
-            memcpy_far(data_addr, bank_get_rel(8), load_data, 0, (sizeof(uint8_t)*SYSDAT_SIZE));
+            fputs("Error reading file", stderr);
+            fclose(fptr);
+            free(load_data);
+            return 0;
         }
-    }
+        
+        data_addr -= RECORD_SIZE;       /* adjust location for writing data */
+        --mpm_records;                  /* adjust number of records remaining */
 
-    if( ((uint16_t)data_addr)%SYSDAT_SIZE != 0 )
-    {
-        /* copy the file remnants from current Bank into Bank 8, from address in file */
-        memcpy_far(data_addr, bank_get_rel(8), &load_data[((uint16_t)data_addr)%SYSDAT_SIZE], 0, ((sizeof(uint8_t)*SYSDAT_SIZE)-((uint16_t)data_addr)%SYSDAT_SIZE) );
-     }
+        /* copy from current Bank into Bank 8, from address in file */
+        memcpy_far(data_addr, -(bank_get_rel(8)), load_data, 0, (sizeof(uint8_t)*RECORD_SIZE));
 
+        printf( "memcpy_far %x %d\n", data_addr, -(bank_get_rel(8)) );
+
+    } while( mpm_records != 0 );
 
     /* Close file */
 
@@ -112,7 +120,7 @@ char ** argv;
         memcpy((void *)page0_template, (void *)0x0000, PAGE0_SIZE);
 
         /* do the FAR copy to the new bank */
-        memcpy_far((void *)0x0000, bank_get_rel(8), page0_template, 0, PAGE0_SIZE);
+        memcpy_far((void *)0x0000, -(bank_get_rel(8)), page0_template, 0, PAGE0_SIZE);
 
         /* set bank referenced from _bankLockBase, so the the bank is noted as warm. */
         lock_give( &bankLockBase[8] );
